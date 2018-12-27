011caabf9f9cbb1e92bb768325f6f7e999e539a2
nvdinh185 <nvdinh0905671805@gmail.com>
2018-12-27 08:41:19 +0700

    push to git

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "push to git"
-git remote add origin https://github.com/nvdinh185/ionic-chat-viber.git
+#git remote add origin https://github.com/nvdinh185/ionic-chat-viber.git
 git push -u origin master
